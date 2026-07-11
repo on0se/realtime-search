0bd130834239121644dff9b33cfff5b3b62b4b15
onose <junjun1h06@gmail.com>
2026-07-11 15:40:19 +0900

    Initial commit

---
+++ src/main/java/com/example/realtimesearch/RealtimeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/realtimesearch/RealtimeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
