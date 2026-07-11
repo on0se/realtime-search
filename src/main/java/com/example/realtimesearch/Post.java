a81d7ed9d687feb6e54e2c913afd7422ef59d6c1
onose <junjun1h06@gmail.com>
2026-07-11 18:12:57 +0900

    Add Post entity class

---
+++ src/main/java/com/example/realtimesearch/Post.java
@@ -0,0 +1,54 @@
+package com.example.realtimesearch;
+import java.time.LocalDateTime;
+
+public class Post {
+
+    private Long id;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+
+    public Post() {
+    }
+
+    public Post(Long id, String content, String author, LocalDateTime createdAt) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+        this.createdAt = createdAt;
+    }
+
+    // ---Getter / Setter ---
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
